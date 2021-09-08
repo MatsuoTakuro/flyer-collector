f8ee8b2dee908919906fe1bb169a09a1542c4299
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-09-08 10:12:56 +0900

    Get all store urls in pagination

--- main.go
+++ main.go
@@ -3,69 +3,76 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const (
-	// TODO:コマンドラインから入力するように変更する（"福岡県"は40へ自動変換 or コマンドライン上で選択）
-	fukuoka int    = 40
-	rawName string = "ディスカウントドラッグコスモス"
-)
-
-func ExampleScrape() {
-	// Set the target url
-	storeName := url.QueryEscape(rawName)
-	sc_url := fmt.Sprintf("https://tokubai.co.jp/%v/prefectures/%d", storeName, fukuoka)
-
-	// Request the HTML page.
-	res, err := http.Get(sc_url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Get the title of this page
-	title := doc.Find("title").Text()
-	fmt.Println("Page title: " + title)
-
-	// Find the review items
-	doc.Find("section.shop_index_cards a").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the store url
-		// TODO: ページネーションのリンクはさらに展開して店舗URLを取得するようにする
-		href, _ := s.Attr("href")
-		burl, _ := url.Parse(sc_url)
-
-		var full_url = toAbsUrl(burl, href)
-		fmt.Printf("In-page link URL #%d : %v\n", i+1, full_url)
-		// TODO: チラシ画像を取得する
-		// TODO: OCRでスキャンする、整理する
-
-		// TODO: ファイルに保存する
-	})
-
+type Store struct {
+	name      string
+	storeURL  string
+	flyerImgs []string
 }
 
-func toAbsUrl(baseurl *url.URL, weburl string) string {
-	relurl, err := url.Parse(weburl)
+const (
+	// TODO:コマンドラインから入力するように変更する（"福岡県"は40へ自動変換 or コマンドライン上で選択）
+	baseURL string = "https://tokubai.co.jp"
+	rawName string = "ディスカウントドラッグコスモス"
+	fukuoka int    = 40
+)
+
+func scrapeTokubai() {
+	// Set the target url
+	storeName := url.QueryEscape(rawName)
+	sc_url := fmt.Sprintf("%v/%v/prefectures/%d", baseURL, storeName, fukuoka)
+
+	// Request the HTML page.
+	resBody := request(sc_url)
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resBody)
+	resBody.Close()
 	if err != nil {
-		return ""
+		log.Fatal(err)
 	}
-	absurl := baseurl.ResolveReference(relurl)
-	return absurl.String()
+
+	var title string
+	var stores []Store
+	title, stores = scrapeThisPage(doc, sc_url)
+	// TODO: チラシ画像を取得する
+	// TODO: OCRでスキャンする
+	// TODO: スキャンされた情報を整形する
+	// TODO: ファイルに保存する
+
+	// Go to next page
+	href, exists := doc.Find("span.next a").Attr("href")
+	for exists {
+		// Set the target url
+		next_sc_url := toAbsUrl(sc_url, href)
+
+		// Request the HTML page.
+		resBody := request(next_sc_url)
+
+		// Load the HTML document
+		doc, err := goquery.NewDocumentFromReader(resBody)
+		resBody.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, tmpStores := scrapeThisPage(doc, sc_url)
+		stores = append(stores, tmpStores...)
+		href, exists = doc.Find("span.next a").Attr("href")
+	}
+
+	fmt.Printf("Page title: %v\n", title)
+	for i, s := range stores {
+		fmt.Printf("Store name #%d : %v\n", i+1, s.name)
+		fmt.Printf("Store URL  #%d : %v\n\n", i+1, s.storeURL)
+	}
+
 }
 
 func main() {
-	ExampleScrape()
+	scrapeTokubai()
 }
---
+++ sub.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func request(sc_url string) io.ReadCloser {
+	// Reduce the burden on the target server
+	time.Sleep(1 * time.Second)
+
+	// Request the HTML page.
+	res, err := http.Get(sc_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	return res.Body
+}
+
+func scrapeThisPage(doc *goquery.Document, sc_url string) (string, []Store) {
+	// Get the title of this page
+	title := doc.Find("title").Text()
+
+	// Find the items
+	var stores []Store
+
+	doc.Find("a.shop_index_card").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the store url
+		name := strings.TrimSpace(s.Find("div.name_text").Text())
+		href, _ := s.Attr("href")
+		storeURL := toAbsUrl(sc_url, href)
+		store := Store{name, storeURL, nil}
+		stores = append(stores, store)
+	})
+	return title, stores
+}
+
+func toAbsUrl(sc_url string, weburl string) string {
+	baseurl, _ := url.Parse(sc_url)
+	relurl, err := url.Parse(weburl)
+	if err != nil {
+		return ""
+	}
+	absurl := baseurl.ResolveReference(relurl)
+	return absurl.String()
+}
